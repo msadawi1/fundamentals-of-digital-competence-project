da3cbdbbec9b363f759d6bb812db6a8c89a1502a
MSadawi <sadawe147@gmail.com>
2025-09-28 19:23:51 +0800

    Initial commit: file boilerplate complete

---
+++ Printing.java
@@ -0,0 +1,14 @@
+
+public class Printing {
+    // shapes variables (values from user)
+    static boolean start = true;
+    static final double PI = 3.14;
+    static double radius;
+    static double length;
+    static double height;
+    static double Total;
+
+    public static void main(String[] args) {
+        
+    }
+}
