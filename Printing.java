2c34276ba077585444b577a7460aff7ccb06e90a
MSadawi <sadawe147@gmail.com>
2025-09-28 19:25:07 +0800

    Add program comment for program purpose clarity

--- Printing.java
+++ Printing.java
@@ -1,4 +1,21 @@
 
+/**
+ * This program calculates the volume of various 3D shapes
+ * (sphere, pyramid, cone, and cylinder) to determine the
+ * amount of resin required for 3D printing.
+ *
+ * Users can input the dimensions of the desired shape,
+ * and the program will compute the corresponding volume,
+ * which represents the estimated resin needed.
+ *
+ * Formulas used:
+ *  - Sphere: (4/3) * π * r^3
+ *  - Pyramid (square base): (l^2 * h) / 3
+ *  - Cone: (π * r^2 * h) / 3
+ *  - Cylinder: π * r^2 * h
+ */
+
+
 public class Printing {
     // shapes variables (values from user)
     static boolean start = true;
@@ -9,6 +26,6 @@ public class Printing {
     static double Total;
 
     public static void main(String[] args) {
-        
+
     }
 }
